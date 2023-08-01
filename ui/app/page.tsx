65e3e056d53c565f88dcfb5a66d9523c2b944ad1
Hello-Misha <94431908+Hello-Misha@users.noreply.github.com>
2023-08-01 15:56:02 +0300

    Hero section image fixed

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -5,6 +5,15 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Найпопулярніші рецепти</h1>
+          <p>
+            Ось список наших найпопулярніших рецептів, які завоювали серця наших
+            користувачів.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
--- ui/components/Footer/Footer.tsx
+++ ui/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import { footerLinks } from "@/constants/index";
 const Footer = () => {
   return (
     <footer className="w-full flex flex-col text-black-100  mt-5 border-t border-gray-100">
-      <div className="mx-auto flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+      <div className="max-w-[1440px] mx-auto flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
             src="/logo.svg"
